4811d494ebe0166ed2b54e1959e9329d1435d6ec
yazhuy <85384621+yazhuy@users.noreply.github.com>
2022-05-04 18:41:47 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,7 +14,7 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo  Successfully Installed !, If the RDP is Dead, Please Rebuild Again! 
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+
 echo Username: administrator
 echo Password: @AviRDP21
 echo .
